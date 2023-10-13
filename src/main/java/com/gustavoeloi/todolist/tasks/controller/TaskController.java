aa2929128fb085f2ed6328feea21477682afd8e7
Gustavo Eloi <gustavooeloi@gmail.com>
2023-10-13 10:39:41 -0300

    feat(controller): validando a data de inicio/termino da task

--- src/main/java/com/gustavoeloi/todolist/tasks/controller/TaskController.java
+++ src/main/java/com/gustavoeloi/todolist/tasks/controller/TaskController.java
@@ -4,8 +4,11 @@ import com.gustavoeloi.todolist.tasks.model.TaskModel;
 import com.gustavoeloi.todolist.tasks.repository.TaskRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 
@@ -17,12 +20,25 @@ public class TaskController {
     TaskRepository taskRepository;
 
     @PostMapping("/")
-    public TaskModel create(@RequestBody TaskModel taskModel, HttpServletRequest request){
+    public ResponseEntity create(@RequestBody TaskModel taskModel, HttpServletRequest request){
         var requestUserId = request.getAttribute("userId");
-
         taskModel.setUserId((UUID) requestUserId);
+
+        LocalDateTime currentDate = LocalDateTime.now();
+        if(currentDate.isAfter(taskModel.getStartAt()) || currentDate.isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de início / término da tarefa não pode ser menor que a data atual");
+        }
+
+        if(taskModel.getStartAt().isAfter(taskModel.getEndAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de ínicio deve ser antes da data de término.");
+        }
+
+        if(taskModel.getEndAt().isBefore(taskModel.getStartAt())){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("A data de término deve ser depois da data de ínicio.");
+        }
+
         var task = this.taskRepository.save(taskModel);
-        return task;
+        return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
 }
